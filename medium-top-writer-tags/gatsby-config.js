a34a58b74066387dd180ed4b4bd0bd3cb926d3ec
Omar Sharaki <omarsharaki@gmail.com>
2020-12-31 21:28:03 +0100

    :tada: Create Gatsby site

---
+++ medium-top-writer-tags/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Medium Top Writer Tags",
+  },
+  plugins: [
+    /* {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    }, */
+    "gatsby-plugin-react-helmet",
+  ],
+};
